d2d866fd73d0e86b799bd7475b8582103b6a59b8
CoddingCatPL <150941284+CoddingCatPL@users.noreply.github.com>
2025-03-11 17:58:23 +0100

    Test again
    a

--- ToggleMute/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ToggleMute
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "ToggleMute";
-        public const string PLUGIN_NAME = "ToggleMute";
-        public const string PLUGIN_VERSION = "0.0.5";
-    }
-}
